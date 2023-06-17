d1ef315d1e266d5d351ea2895bfad6e26d460de8
Ahmed Fekry <32654039+AhmedUFekry@users.noreply.github.com>
2023-06-17 11:26:36 -0700

    Add files via upload

---
+++ TeaComputersTechnicalTask/TeaComputersTechnicalTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeaComputersTechnicalTask
+//
+//  Created by Ahmed Fekry on 17/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
